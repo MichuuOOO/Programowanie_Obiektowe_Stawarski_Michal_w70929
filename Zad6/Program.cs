a29f5ee4330407813d674d4855c9ea12627e61bb
Michał Stawarski <151212530+MichuuOOO@users.noreply.github.com>
2024-12-11 18:28:33 +0100

    Zad6
    Dodano zad6

---
+++ Zad6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Wprowadź liczby całkowite. Wprowadzenie liczby ujemnej zakończy program: ");
+
+        for (; ; )
+        {
+            Console.Write("Podaj liczbę całkowitą: ");
+            int liczba = Convert.ToInt32(Console.ReadLine());
+
+            if (liczba < 0)
+            {
+                Console.WriteLine("Wprowadzono liczbę ujemną. Koniec programu.");
+                break;
+            }
+
+            Console.WriteLine($"Wprowadzono liczbę: {liczba}");
+        }
+    }
+}
