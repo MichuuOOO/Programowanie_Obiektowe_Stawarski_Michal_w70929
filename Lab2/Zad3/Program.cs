7ee75ba3a552f268fff1b4c77637855f17a7d96f
Michał Stawarski <151212530+MichuuOOO@users.noreply.github.com>
2025-01-26 21:54:22 +0100

    Dodano Zad3

---
+++ Lab2/Zad3/Program.cs
@@ -0,0 +1,6 @@
+﻿using Zad3;
+
+var student = new Student("Michał", "Stawarski");
+student.DodajOcene(5);
+student.DodajOcene(3);
+student.WyswietlInformacje();
---
+++ Lab2/Zad3/Student.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Zad3
+{
+    internal class Student
+    {
+        public string Imie { get; private set; }
+        public string Nazwisko { get; private set; }
+        private List<int> Oceny { get; set; }
+
+        public double SredniaOcen => Oceny.Count > 0 ? Oceny.Average() : 0;
+
+        public Student(string imie, string nazwisko)
+        {
+            Imie = imie;
+            Nazwisko = nazwisko;
+            Oceny = new List<int>();
+        }
+
+        public void DodajOcene(int ocena)
+        {
+            if (ocena < 1 || ocena > 6)
+                throw new ArgumentException("Ocena musi być w zakresie od 1 do 6.");
+            Oceny.Add(ocena);
+        }
+
+        public void WyswietlInformacje()
+        {
+            Console.WriteLine($"Imię: {Imie}, Nazwisko: {Nazwisko}, Średnia ocen: {SredniaOcen:F2}");
+        }
+    }
+}
+
