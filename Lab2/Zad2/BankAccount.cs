9270aede25e47ecc8c96f8ab63ce012257c35cae
Michał Stawarski <151212530+MichuuOOO@users.noreply.github.com>
2025-01-26 21:53:46 +0100

    Dodano zad2

---
+++ Lab2/Zad2/BankAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Zad2
+{
+    internal class BankAccount
+    {
+        public string Wlasciciel { get; private set; }
+        public decimal Saldo { get; private set; }
+
+        public BankAccount(string wlasciciel, decimal poczatkoweSaldo)
+        {
+            Wlasciciel = wlasciciel;
+            Saldo = poczatkoweSaldo > 0 ? poczatkoweSaldo : throw new ArgumentException("Saldo początkowe musi być większe od zera.");
+        }
+
+        public void Wplata(decimal kwota)
+        {
+            if (kwota <= 0)
+                throw new ArgumentException("Kwota wpłaty musi być dodatnia.");
+            Saldo += kwota;
+        }
+
+        public void Wyplata(decimal kwota)
+        {
+            if (kwota <= 0)
+                throw new ArgumentException("Kwota wypłaty musi być dodatnia.");
+            if (kwota > Saldo)
+                throw new InvalidOperationException("Niewystarczająca ilość środków.");
+            Saldo -= kwota;
+        }
+    }
+}
---
+++ Lab2/Zad2/Program.cs
@@ -0,0 +1,6 @@
+﻿using Zad2;
+
+var konto = new BankAccount("Robert Lewandowski", 5000);
+konto.Wplata(300);
+konto.Wyplata(100);
+Console.WriteLine($"Saldo: {konto.Saldo}");
