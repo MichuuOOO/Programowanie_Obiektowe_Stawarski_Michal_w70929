81132308889e2c7c3ca23fd0904aedf8d98aaf35
Michał Stawarski <151212530+MichuuOOO@users.noreply.github.com>
2025-01-26 21:55:05 +0100

    Dodano Zad4

---
+++ Lab2/Zad4/Licz.cs
@@ -0,0 +1,27 @@
+﻿namespace Zad4
+{
+    internal class Licz
+    {
+        private int value;
+
+        public Licz(int initialValue)
+        {
+            value = initialValue;
+        }
+
+        public void Dodaj(int liczba)
+        {
+            value += liczba;
+        }
+
+        public void Odejmij(int liczba)
+        {
+            value -= liczba;
+        }
+
+        public void WyswietlStan()
+        {
+            Console.WriteLine($"Wartość: {value}");
+        }
+    }
+}
---
+++ Lab2/Zad4/Program.cs
@@ -0,0 +1,5 @@
+﻿using Zad4;
+var licz = new Licz(10);
+licz.Dodaj(5);
+licz.Odejmij(3);
+licz.WyswietlStan();
