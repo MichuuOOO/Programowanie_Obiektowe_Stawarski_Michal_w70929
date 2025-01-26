4b2f109ab6392e94f80a98117a664e6132c6670c
Michał Stawarski <151212530+MichuuOOO@users.noreply.github.com>
2025-01-26 21:55:59 +0100

    Dodano Zad5

---
+++ Lab2/Zad5/Program.cs
@@ -0,0 +1,7 @@
+﻿using Zad5;
+var sumator = new Sumator(new int[] { 1, 2, 3, 4, 5 });
+Console.WriteLine($"Suma: {sumator.Suma()}");
+Console.WriteLine($"Suma liczb podzielnych przez 2: {sumator.SumaPodziel2()}");
+Console.WriteLine($"Ilość elementów: {sumator.IleElementow()}");
+sumator.WypiszElementy();
+sumator.WypiszZakres(1, 3);
---
+++ Lab2/Zad5/Sumator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad5
+{
+    internal class Sumator
+    {
+        private int[] Liczby;
+
+        public Sumator(int[] liczby)
+        {
+            Liczby = liczby ?? throw new ArgumentNullException(nameof(liczby));
+        }
+
+        public int Suma()
+        {
+            int suma = 0;
+            foreach (var liczba in Liczby)
+            {
+                suma += liczba;
+            }
+            return suma;
+        }
+
+        public int SumaPodziel2()
+        {
+            int suma = 0;
+            foreach (var liczba in Liczby)
+            {
+                if (liczba % 2 == 0)
+                    suma += liczba;
+            }
+            return suma;
+        }
+
+        public int IleElementow()
+        {
+            return Liczby.Length;
+        }
+
+        public void WypiszElementy()
+        {
+            Console.WriteLine(string.Join(", ", Liczby));
+        }
+
+        public void WypiszZakres(int lowIndex, int highIndex)
+        {
+            for (int i = Math.Max(0, lowIndex); i <= Math.Min(Liczby.Length - 1, highIndex); i++)
+            {
+                Console.Write(Liczby[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
