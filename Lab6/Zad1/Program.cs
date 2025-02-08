517658dd4c6322e89e659773f18b594779da9adf
Michał Stawarski <151212530+MichuuOOO@users.noreply.github.com>
2025-02-08 21:06:45 +0100

    Dodano Lab6/Zad1

---
+++ Lab6/Zad1/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Podaj nazwę pliku: ");
+        string fileName = Console.ReadLine();
+
+        Console.Write("Podaj swój numer albumu: ");
+        string numerAlbumu = Console.ReadLine();  
+
+        try
+        {
+            File.WriteAllText(fileName, numerAlbumu);
+            Console.WriteLine($"Numer albumu zapisano do pliku {fileName}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Wystąpił błąd: {e.Message}");
+        }
+    }
+}
