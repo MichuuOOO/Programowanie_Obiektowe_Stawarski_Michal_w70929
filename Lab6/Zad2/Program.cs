8480fbbf2175e1421c0752462d91fbab50dd43e6
Michał Stawarski <151212530+MichuuOOO@users.noreply.github.com>
2025-02-08 21:09:55 +0100

    Dodano Zad2

---
+++ Lab6/Zad2/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Podaj nazwę pliku ktory chcesz odczytac: ");
+        string fileName = Console.ReadLine();  
+
+        if (File.Exists(fileName))  
+        {
+            try
+            {
+                string zawartosc = File.ReadAllText(fileName);  
+                Console.WriteLine("Zawartość pliku:");
+                Console.WriteLine(zawartosc);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Błąd odczytu pliku: {e.Message}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Plik nie istnieje.");
+        }
+    }
+}
