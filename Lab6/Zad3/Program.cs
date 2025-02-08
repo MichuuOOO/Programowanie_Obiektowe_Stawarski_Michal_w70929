2c643998da01fba600784d72cfdb3b42d7a68209
Michał Stawarski <151212530+MichuuOOO@users.noreply.github.com>
2025-02-08 21:10:26 +0100

    Dodano Zad3

---
+++ Lab6/Zad3/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string fileName = "pesels.txt";  
+
+        if (!File.Exists(fileName))  
+        {
+            Console.WriteLine("Plik pesels.txt nie istnieje!");
+            return;
+        }
+
+        try
+        {
+            string[] pesels = File.ReadAllLines(fileName);  
+            int liczbaKobiet = pesels.Count(pesel => pesel.Length == 11 && pesel[9] % 2 == 0);
+
+            Console.WriteLine($"Liczba kobiet: {liczbaKobiet}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Błąd odczytu pliku: {e.Message}");
+        }
+    }
+}
