06fe47f89fa6fa840b6e17abf2c95ea70ef509a7
Michał Stawarski <151212530+MichuuOOO@users.noreply.github.com>
2025-02-08 11:23:33 +0100

    Dodano Zad3

---
+++ Lab5/Zad3/Kolor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad3
+{
+    public enum Kolor
+    {
+        Czerwony,
+        Niebieski,
+        Zielony,
+        Żółty,
+        Fioletowy
+    }
+}
---
+++ Lab5/Zad3/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Zad3
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<Kolor> dostepneKolory = new()
+            {
+                Kolor.Czerwony,
+                Kolor.Niebieski,
+                Kolor.Zielony,
+                Kolor.Żółty,
+                Kolor.Fioletowy
+            };
+
+            Random rand = new();
+            Kolor wybranyKolor = dostepneKolory[rand.Next(dostepneKolory.Count)];
+
+            bool trafiono = false;
+            Console.WriteLine("Gra w zgadywanie kolorów");
+            Console.WriteLine("Dostępne kolory: Czerwony, Niebieski, Zielony, Żółty, Fioletowy");
+
+            while (!trafiono)
+            {
+                Console.Write("Zgadnij kolor: ");
+                string wpis = Console.ReadLine();
+
+                try
+                {
+                    if (!Enum.TryParse<Kolor>(wpis, true, out Kolor zgadywanyKolor) ||
+                        !dostepneKolory.Contains(zgadywanyKolor))
+                    {
+                        throw new ArgumentException("Wprowadzony kolor nie znajduje się na liście.");
+                    }
+
+                    if (zgadywanyKolor == wybranyKolor)
+                    {
+                        Console.WriteLine("Gratulacje! Zgadłes prawidłowy kolor.");
+                        trafiono = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Niestety, nie zgadles koloru. Spróbuj ponownie.");
+                    }
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine($"Błąd: {ex.Message}");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Dzięki za grę!");
+        }
+    }
+}
